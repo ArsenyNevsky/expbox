cf8be2815d2566fe3571ca702dadc6ad71320f1d
Arseny <arkaim@MacStudio-Arseny.local>
2026-03-27 14:56:44 +0300

    first_commit

--- src/main/java/ru/garage/nevars/expbox/ExpboxApplication.java
+++ src/main/java/ru/garage/nevars/expbox/ExpboxApplication.java
@@ -3,11 +3,20 @@ package ru.garage.nevars.expbox;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.ZoneId;
+import java.util.*;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class ExpboxApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExpboxApplication.class, args);
+        //SpringApplication.run(ExpboxApplication.class, args);
+        Set<String> ids = Set.of("a", "b", "c");
+        Set<String> found = Set.of("b");
+        Set<String> diff = new HashSet<>(ids);
+        diff.removeAll(found);
+        System.out.println(diff);
 	}
 
 }
